c00aa46aea8d752251c40b8d67f3baa020517027
zhaodengfeng <zhaodengfeng@users.noreply.github.com>
2026-04-11 19:42:23 +0800

    Bump version to 0.2.9

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -21,7 +21,7 @@ import (
 	"github.com/zhaodengfeng/dtsw/internal/wizard"
 )
 
-const Version = "0.2.8"
+const Version = "0.2.9"
 
 func Run(args []string, stdout, stderr io.Writer) int {
 	if len(args) < 2 {
